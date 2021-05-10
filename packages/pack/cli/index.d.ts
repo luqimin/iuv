a769fe06321cf6e6bc5ab2039febd1789802ffe9
QiminLu <qilu@163.com>
2021-05-11 01:59:54 +0800

    fix: 删除无用文件

--- packages/pack/cli/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*package.json' {
-    export const name: string;
-    export const version: string;
-}
-
-declare module 'download-git-repo';
--- packages/pack/cli/src/utils/version.ts
+++
@@ -1,5 +0,0 @@
-/**
- * 获取iuv版本号
- */
-
-export { version as localVersion } from '../../package.json';
